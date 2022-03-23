b1d546371509399b83aaa6b2617d05800c113f1e
Bloxxel64 <70494679+Bloxxel64@users.noreply.github.com>
2022-03-23 17:33:18 -0500

    Add files via upload
    i forgor :skull:

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+until [[ ${ANSWER,,} == "pacman" ]] || [[ ${ANSWER,,} == "apt" ]]; do
+	read -p "Use pacman or apt? " ANSWER
+done
+
+if [[ ${ANSWER,,} == "pacman" ]]; then
+	sudo pacman -S python3 python-pip tk sdl2_gfx sdl2_image sdl2_mixer sdl2_net git
+else
+	sudo apt-get install -y python3 python3-pip python3-tk libsdl2-dev git
+	pip3 install pysimplegui
+fi
+
+pip3 install pysimplegui
+cp -R src ~/SM64LinuxLauncher
